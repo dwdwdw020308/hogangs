705383cbb0ae2f2679e7ed46b5b443acd6ced874
dwdwdw020308 <115512931+LeeJoNyun@users.noreply.github.com>
2025-09-09 15:37:23 +0900

    빌드 실패 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { Auth, AuthCallback, Main, Ott } from './pages';
-import Layout from './common/Layout';
-import './styles/index.scss';
-import AboutUs from './pages/About';
-import MyPage from './pages/mypage';
-import Videos from './pages/videos';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Auth, AuthCallback, Main, MyPage, Ott } from "./pages";
+import Layout from "./common/Layout";
+import "./styles/index.scss";
+import AboutUs from "./pages/About";
+
+import Videos from "./pages/videos";
 
 const App = () => {
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Main />} />
-                        <Route path="/ott" element={<Ott />} />
-                        <Route path="about" element={<AboutUs />} />
-                        <Route path="mypage" element={<MyPage />} />
-                        <Route path="videos" element={<Videos />} />
-                    </Route>
-                    <Route path="auth" element={<Auth />} />
-                    <Route path="authcallback" element={<AuthCallback />} />
-                </Routes>
-            </BrowserRouter>
-        </>
-    );
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Main />} />
+            <Route path="/ott" element={<Ott />} />
+            <Route path="about" element={<AboutUs />} />
+            <Route path="mypage" element={<MyPage />} />
+            <Route path="videos" element={<Videos />} />
+          </Route>
+          <Route path="auth" element={<Auth />} />
+          <Route path="authcallback" element={<AuthCallback />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
-export { default as Main } from './main/Main';
-export { default as Ott } from './ott/Ott';
-export { default as About } from './About/index';
-export { default as AuthCallback } from './auth/AuthCallback';
-export { default as Auth } from './auth/Auth';
+export { default as Main } from "./main/Main";
+export { default as Ott } from "./ott/Ott";
+export { default as About } from "./About/index";
+export { default as AuthCallback } from "./auth/AuthCallback";
+export { default as Auth } from "./auth/Auth";
+export { default as MyPage } from "./MyPage";
