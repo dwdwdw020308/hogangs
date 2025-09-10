0c65fdeae22bf8aa20d1ea8c7ee9bfe18d8fae54
gkstmftt <yanglily328@gmail.com>
2025-09-10 11:40:53 +0900

    0910한슬수정2

--- src/components/ott/Visual.jsx
+++ src/components/ott/Visual.jsx
@@ -11,10 +11,26 @@ const posters = [
     '/ott/movie10.png',
 ];
 
+const posters = [
+    '/ott/movie1.png',
+    '/ott/movie2.png',
+    '/ott/movie3.png',
+    '/ott/movie4.png',
+    '/ott/movie5.png',
+    '/ott/movie6.png',
+    '/ott/movie7.png',
+    '/ott/movie8.png',
+    '/ott/movie9.png',
+    '/ott/movie10.png',
+];
+
 const Visual = () => {
     return (
         <div className="ottmain-visual">
             <div className="title">
+                <strong>
+                    소중한 <span>반려견과 함께</span>하는 달콤한 <span>휴식</span>
+                </strong>
                 <strong>
                     소중한 <span>반려견과 함께</span>하는 달콤한 <span>휴식</span>
                 </strong>
