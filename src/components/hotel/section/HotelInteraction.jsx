0b9c03532ce4f60e08f73993483b11a682363f2a
이준현 <115512931+LeeJoNyun@users.noreply.github.com>
2025-09-10 21:26:40 +0900

    호텔 수직 롤링 이미지 작업 완료

--- src/components/hotel/section/HotelInteraction.jsx
+++ src/components/hotel/section/HotelInteraction.jsx
@@ -1,37 +1,36 @@
-const col1 = ["/dogs/1.jpg", "/dogs/2.jpg", "/dogs/3.jpg", "/dogs/4.jpg"];
-const col2 = ["/dogs/5.jpg", "/dogs/6.jpg", "/dogs/7.jpg", "/dogs/8.jpg"];
-const col3 = ["/dogs/9.jpg", "/dogs/10.jpg", "/dogs/11.jpg", "/dogs/12.jpg"];
+const col = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png'];
 
 const HotelInteraction = () => {
-  return (
-    <section id="hotel_interaction">
-      <div className="rolling_area">
-        <div className="col up">
-          <div className="track">
-            {[...col1, ...col1].map((src, i) => (
-              <img key={i} src={src} alt="" />
-            ))}
-          </div>
-        </div>
+    const srcUrl = '/hotel/roll';
+    return (
+        <section id="hotel_interaction">
+            <div className="rolling_area">
+                <div className="col up">
+                    <div className="track">
+                        {[...col, ...col].map((src, i) => (
+                            <img key={i} src={srcUrl + '/col1/' + src} alt="" />
+                        ))}
+                    </div>
+                </div>
 
-        <div className="col down">
-          <div className="track">
-            {[...col2, ...col2].map((src, i) => (
-              <img key={i} src={src} alt="" />
-            ))}
-          </div>
-        </div>
+                <div className="col down">
+                    <div className="track">
+                        {[...col, ...col].map((src, i) => (
+                            <img key={i} src={srcUrl + '/col2/' + src} alt="" />
+                        ))}
+                    </div>
+                </div>
 
-        <div className="col up">
-          <div className="track">
-            {[...col3, ...col3].map((src, i) => (
-              <img key={i} src={src} alt="" />
-            ))}
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+                <div className="col up">
+                    <div className="track">
+                        {[...col, ...col].map((src, i) => (
+                            <img key={i} src={srcUrl + '/col3/' + src} alt="" />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default HotelInteraction;
--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,17 +1,19 @@
-import CareService from "../../components/hotel/section/CareService";
-import Price from "../../components/hotel/section/Price";
-import Reservation from "../../components/hotel/section/Reservation";
-import Visual from "../../components/hotel/Visual";
+import CareService from '../../components/hotel/section/CareService';
+import HotelInteraction from '../../components/hotel/section/HotelInteraction';
+import Price from '../../components/hotel/section/Price';
+import Reservation from '../../components/hotel/section/Reservation';
+import Visual from '../../components/hotel/Visual';
 
 const Hotel = () => {
-  return (
-    <>
-      <Visual />
-      <Reservation />
-      <CareService />
-      {/* <Price /> */}
-    </>
-  );
+    return (
+        <>
+            <Visual />
+            <Reservation />
+            <CareService />
+            {/* <Price /> */}
+            <HotelInteraction />
+        </>
+    );
 };
 
 export default Hotel;
