6f947fd6df9a9dbc7212fe5e04dabe3ae1b946c7
LeeJoNyun <115512931+LeeJoNyun@users.noreply.github.com>
2025-09-11 18:20:23 +0900

    0911 커밋

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,19 +1,21 @@
-import CareService from '../../components/hotel/section/CareService';
-import HotelInteraction from '../../components/hotel/section/HotelInteraction';
-import Price from '../../components/hotel/section/Price';
-import Reservation from '../../components/hotel/section/Reservation';
-import Visual from '../../components/hotel/Visual';
+import CareService from "../../components/hotel/section/CareService";
+import HotelInteraction from "../../components/hotel/section/HotelInteraction";
+import IntroduceService from "../../components/hotel/section/IntroduceService";
+import Price from "../../components/hotel/section/Price";
+import Reservation from "../../components/hotel/section/Reservation";
+import Visual from "../../components/hotel/Visual";
 
 const Hotel = () => {
-    return (
-        <>
-            <Visual />
-            <Reservation />
-            <CareService />
-            {/* <Price /> */}
-            <HotelInteraction />
-        </>
-    );
+  return (
+    <>
+      <Visual />
+      <Reservation />
+      <IntroduceService />
+      <CareService />
+      {/* <Price /> */}
+      <HotelInteraction />
+    </>
+  );
 };
 
 export default Hotel;
